4b29017cdcb77f6d8d50bae868e4b5cf448fd54f
RaphaelBaumann <66304074+RaphaelBaumann@users.noreply.github.com>
2022-08-11 13:38:53 +0200

    testing

--- entrypoint-github.sh
+++ entrypoint-github.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 set -e
+echo "hello 1"
 
 export HOME=/cli  # To overwrite GitHub run command
 
+echo "hello 2"
+
 cd $HOME
+echo "hello 3"
 IFS=" " read -r -a args <<< "$@"
+echo "hello 4"
 response=$(/cli/entrypoint.sh "${args[@]}")
 echo "::set-output name=output::$response"
