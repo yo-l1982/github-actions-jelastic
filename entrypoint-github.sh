2903aa2c03a2c80cc8f4d32c3789a44970076a2c
RaphaelBaumann <66304074+RaphaelBaumann@users.noreply.github.com>
2022-08-11 14:30:34 +0200

    clean up

--- entrypoint-github.sh
+++ entrypoint-github.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 set -e
-echo "hello 1"
 
 export HOME=/cli  # To overwrite GitHub run command
 
-echo "hello 2"
-
 cd $HOME
-echo "hello 3"
+
 IFS=" " read -r -a args <<< "$@"
-echo "hello 4"
+
 response=$(/cli/entrypoint.sh "${args[@]}")
-echo "hello 5"
+
 echo "::set-output name=output::$response"
-echo "hello 6"
-exit 1
