a09ee8b68224c82a30a6991285525923265280a5
WAYOFFprod <raphael@wayoff.ch>
2022-08-25 15:51:46 +0200

    add logs for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,12 @@ home="/cli/jelastic"
 
 if [ "$login" == "true" ]
 then
+  echo "should be logging in"
   echo n | $home/users/authentication/signin --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL"
 fi
+echo "pre response"
 response=$("$home/$command" "${@:3}")
-
+echo "post response"
 # if [ "$json" == "true" ]
 # then
 #   response=$(echo "$response" | sed -n '1!p' | jq --compact-output || echo "$response")
