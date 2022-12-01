0c4a393b46604a89151d5a7ca401fe41bd962950
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-12-01 11:06:27 +0100

    ci: rearrange command and dont echo login

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ home="/cli/jelastic"
 
 if [ "$login" == "true" ]
 then
-  echo n | $home/users/authentication/signin --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" --silent
+  $home/users/authentication/signin --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" --silent
 fi
 
-response=$("$home/$command" "${@:3}" "--silent") 
+response=$("$home/$command --silent") 
 
 # if [ "$json" == "true" ]
 # then
