c7c4522d5c547e52ffa6289c031964c72bb7740e
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-12-01 11:27:26 +0100

    ci: test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ home="/cli/jelastic"
 
 if [ "$login" == "true" ]
 then
-  /cli/jelastic/users/authentication/signin --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" --silent
+  echo n | $home/users/authentication/signin --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" --silent
 fi
 
-response=$("$home/$command --silent") 
+response=$("$home/$command" "--silent") 
 
 # if [ "$json" == "true" ]
 # then
